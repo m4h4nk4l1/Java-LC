0ecccd5e2698768bdc728fd8ec70e9af11e0dbbc
maha <m4h4nk4l1@gmail.com>
2023-05-29 22:30:01 +0530

    sorting sums done

--- day2/ArrayListExample.java
+++ day2/ArrayListExample.java
@@ -3,7 +3,7 @@ package day2;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ArrayListExample {
+public class fArrayListExample {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         // Syntax
